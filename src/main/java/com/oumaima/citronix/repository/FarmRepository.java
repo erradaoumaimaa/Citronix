c0bbd181844afbb28d3d365839db3cc2348720e3
ER-RADA Oumaima <erradaoumaima@gmail.com>
2024-11-17 02:31:32 +0100

    fixer un erreur dans liquibase

--- src/main/java/com/oumaima/citronix/repository/FarmRepository.java
+++ src/main/java/com/oumaima/citronix/repository/FarmRepository.java
@@ -1,4 +1,10 @@
 package com.oumaima.citronix.repository;
 
-public interface FarmRepository {
+import com.oumaima.citronix.entity.Farm;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.UUID;
+
+public interface FarmRepository extends JpaRepository<Farm, UUID> {
+
 }
